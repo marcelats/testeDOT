1dcb3b57577fd3593d4569d232897de3e76df5b8
Marcela Shinzato <marcelats@usp.br>
2025-07-01 11:58:10 -0300

    descomentei o fetch

--- src/main/java/com/asda/RestApplication.java
+++ src/main/java/com/asda/RestApplication.java
@@ -3,7 +3,7 @@ package com.asda;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/ROOT") // ou "/api" se quiser que os endpoints comecem com /api
+@ApplicationPath("/ROOT/") // ou "/api" se quiser que os endpoints comecem com /api
 public class RestApplication extends Application {
     // vazia: usada apenas para ativar o JAX-RS
 }
